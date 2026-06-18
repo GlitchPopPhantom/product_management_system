5c99da95598100c42f477c158c7694339af63db2
Tunji <macchupiccchu2003@gmail.com>
2026-06-18 14:28:12 +0100

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,8 +4,6 @@ from .views import register_user, ProductListCreate, ProductDetail, CategoryList
 urlpatterns = [
     path('login/', register_user, name='api_login'),
     path('register/', register_user, name='api_register'),
-    
-    # Clean Product Resource Names
     path('products/', ProductListCreate.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
     path('categories/', CategoryListCreate.as_view(), name='category-list-create'),
