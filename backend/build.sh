732b238a78674a1913817dffaf22b748dec3c27d
Tunji <macchupiccchu2003@gmail.com>
2026-06-19 15:02:11 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -2,6 +2,13 @@
 set -o errexit
 
 pip install -r requirements.txt
+
+# Sync production static components
+python manage.py collectstatic --no-input
+
+# Mark migration 0001 as already handled so it skips the crash point
+python manage.py migrate api 0001 --fake --no-input
+
+# Run the rest of the updates smoothly
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-python manage.py collectstatic --no-input
