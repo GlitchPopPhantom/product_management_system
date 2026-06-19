2e5a0657c5c41a9544011885761ed71699e0874c
Tunji <macchupiccchu2003@gmail.com>
2026-06-19 11:27:39 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
-set -o errexit
 
-# 1. Install all dependencies
 pip install -r requirements.txt
 
-# 2. Compile static assets safely without hitting the database link yet
-DATABASE_URL="postgres://dummy:dummy@localhost:5432/dummy" python manage.py collectstatic --no-input
-
-# 3. Create migration files
-python manage.py makemigrations api
-
-# 4. Force migrations directly onto your live database during the build
 python manage.py migrate
+
+python manage.py collectstatic --noinput
