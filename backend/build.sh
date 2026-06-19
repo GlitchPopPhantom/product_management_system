1cea266cc0dfba13df7bf0c18aed66082447ac33
Tunji <macchupiccchu2003@gmail.com>
2026-06-19 12:37:46 +0100

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -3,7 +3,3 @@ set -o errexit
 
 pip install -r requirements.txt
 DATABASE_URL="postgres://dummy:dummy@localhost:5432/dummy" python manage.py collectstatic --no-input
-
-# Wipe and create database structures natively via middleware or startup execution
-# Then populate the tables right away
-python seed_products.py
